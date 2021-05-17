2211bebbb2e1df9f0fe172738671c7fb5a336a10
“wataru” <ltomu.web@gmail.com>
2021-05-17 09:01:51 +0900

    hide secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+try:
+    from .local_settings import SECRET_KEY  as key
+except ImportError:
+    pass
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-36tkt4ki@fj@2l#&&pwdfc2^_ojr3j)^)v=iwnmjf==i3al061'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
