01aba42cda41ab795a176584d62804eacd0bcd91
“wataru” <ltomu.web@gmail.com>
2021-05-19 23:18:53 +0900

    add vote function

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,8 @@
-from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-from django import template
-from polls.models import Question
-from django.shortcuts import render
 # from django.template import context, loader
-from django.http import HttpResponse, response
-from .models import Question
+from django.http import HttpResponseRedirect
+from .models import Choice, Question
+from django.urls import reverse
 
 
 def index(request):
@@ -27,8 +24,19 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on quesiton %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
