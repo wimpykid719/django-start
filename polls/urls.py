35b6e7fe8033117555807f96bd3a01861555b137
“wataru” <ltomu.web@gmail.com>
2021-05-20 10:15:03 +0900

    done tutorial 4

--- polls/urls.py
+++ polls/urls.py
@@ -5,10 +5,10 @@ app_name = 'polls'
 
 urlpatterns = [
     # /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 
     # ex：/polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,31 +1,54 @@
 from django.shortcuts import render, get_object_or_404
 # from django.template import context, loader
 from django.http import HttpResponseRedirect
+from django.template import context
 from .models import Choice, Question
 from django.urls import reverse
+from django.views import generic
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    # テンプレート側でQuestion.objects.order_by('-pub_date')[:5]を呼び出す際の名前を設定している。
+    context_object_name = 'Latest_question_list'
 
-def detail(request, question_id):
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+# 汎用リファクタリング前
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return render(request, 'polls/index.html', context)
+
+class DetailView(generic.DetailView):
+    # テンプレートで変数にアクセスする際はquestionになる。
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+# def detail(request, question_id):
     # try:
     #     question = Question.objects.get(pk=question_id)
     # except Question.DoesNotExist:
     #     raise Http404('Question does not exist')
     # return render(request, 'polls/detail.html', {'question': question})
 
+    # 汎用リファクタリング前
     # 上記を書き換える。
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    # question = get_object_or_404(Question, pk=question_id)
+    # return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+# 汎用リファクタリング前
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
